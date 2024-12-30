238590278af8926d672f2fb696fbc7a714ac9f4d
quaere-verum <=>
2024-12-30 23:30:21 +0100

    Added docstrings

--- src/quantrl/features/bars.py
+++ src/quantrl/features/bars.py
@@ -8,6 +8,28 @@ def calculate_bars(
     bar_size: float,
     aggregate_functions: Iterable[pl.Expr], 
 ) -> pl.DataFrame:
+    """
+    Use time bars to calculate new bars. A new bar is formed once 'bar_size' of 'bar_column_name' has 
+    been registered. Consider e.g. pl.Series('bar_column_name', [0, 1, 0, 1, 2, 0, 3, 0, 1, 1]), with
+    bar_size = 2. Then a new bar will be formed at t=3, 4, 8. That is, when 
+    pl.col('bar_column_name').cum_sum().mod(bar_size) == 0.
+
+    Parameters
+    ----------
+    dataframe : pl.DataFrame
+        The dataframe to transform, with primary key ('market_id', 'symbol_id').
+    bar_column_name : str
+        The column name to aggregate on.
+    bar_size : float
+        The amount to fill before a new bar is generated.
+    aggregate_functions : Iterable[pl.Expr]
+        The aggregation expressions to fill the new dataframe.
+
+    Returns
+    -------
+    pl.DataFrame
+        The aggregated dataframe, with primary key ('bar_id', 'symbol_id').
+    """
     assert np.all(np.isin(["symbol_id", "market_id", bar_column_name], dataframe.columns))
     dataframe = (
         dataframe
--- src/quantrl/features/frac_diff.py
+++ src/quantrl/features/frac_diff.py
@@ -13,6 +13,27 @@ def _weights(d: float, order: int) -> np.ndarray[Any, float]:
     return ((-1) ** k.flatten() * product)[::-1]
 
 def frac_diff(x: np.ndarray[Any, float], d: float, order: int, *, axis: int = 0) -> np.ndarray[Any, float]:
+    """
+    Take the fractional difference of degree d, for the array x along the specified axis. Uses the formula
+    (1 - B)^d = \sum_{k=0}^\infty B (-1)^k\prod_{i=0}^{k-1}(d-i)/(k-i) where B is the backshift operator.
+    The series is expanded up until the specified order.
+
+    Parameters
+    ----------
+    x : np.ndarray[Any, float]
+        Array to be differenced.
+    d : float
+        Degree of differentiation.
+    order : int
+        Order until which the power series is expanded.
+    axis : int, optional
+        Axis along which to difference, by default 0.
+
+    Returns
+    -------
+    np.ndarray[Any, float]
+        The fractionally differenced array.
+    """
     rolling_view = np.lib.stride_tricks.sliding_window_view(
         x,
         window_shape=order + 1,
